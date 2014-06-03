2f2867080fe1dbc54d2b39c3e8f2d60819eec8d7
Rami Ahmed Bock <RamiAhmedBock@gmail.com>
2014-06-03 15:00:21 +0200

    Added base calls to virtual methods

--- Assets/Scripts/FSM_Unit.cs
+++ Assets/Scripts/FSM_Unit.cs
@@ -14,12 +14,12 @@ public class FSM_Unit : Entity {
 
 	// Use this for initialization
 	protected override void Start () {
-	
+		base.Start();
 	}
 	
 	// Update is called once per frame
 	protected override void Update () {
-	
+		base.Update();
 	}
 
 	/*
